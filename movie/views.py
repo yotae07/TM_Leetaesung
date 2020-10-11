f7ac3dacda7eaa0c162b5e4f10742d99a8458742
이태성 <tomleets@MacBook-Pro.local>
2020-10-11 16:53:19 +0900

    Add: make movie get id method views.py

--- movie/views.py
+++ movie/views.py
@@ -20,3 +20,28 @@ class MovieView(View):
                 "year": data.year.year,
             } for data in datas]
         return JsonResponse({"data": result}, status=200)
+
+class MovieDetailView(View):
+    def get(self, request, movie_id):
+        if movie_id == 0:
+            return JsonResponse(
+                    {
+                        "head": "I'm a teapot",
+                        "body": "The requested entity body is short and stout."
+                    }, status=418)
+        if Movie.objects.filter(id=movie_id).exists():
+            datas = Movie.objects.select_related('year', 'genres', 'country').all().order_by('year', 'title')
+            result = [
+                {
+                    "id": data.id,
+                    "title": data.title,
+                    "year": data.year.year,
+                    "rating": data.rating,
+                    "genres": data.genres.genres,
+                    "country": data.country.country,
+                    "runtime": data.runtime,
+                    "summary": data.summary,
+                    "poster": data.poster
+                } for data in datas]
+            return JsonResponse({"data": result}, status=200)
+        return JsonResponse({"message": "INVALID_REQUEST"}, status=400)
