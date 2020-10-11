8c5b98fa6ef73ac3276fc62bfbd75a7a42eb6b76
이태성 <tomleets@MacBook-Pro.local>
2020-10-11 16:53:27 +0900

    Add: make movie get id method urls.py

--- movie/urls.py
+++ movie/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views      import (
     MovieView,
+    MovieDetailView,
 )
 
 urlpatterns = [
     path('', MovieView.as_view()),
+    path('/<int:movie_id>', MovieDetailView.as_view()),
 ]
